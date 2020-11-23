bda74a05a13570e82b1805e7f2f8955eaec522d0
hearecho <1540302560@qq.com>
2020-11-23 11:05:30 +0800

    2020/11/23-每日一题-452

--- dailyProblem/dailyProblen_test.go
+++ dailyProblem/dailyProblen_test.go
@@ -5,47 +5,52 @@ import (
 	"testing"
 )
 
-func Test_ladderLength(t *testing.T)  {
-	fmt.Println(ladderLength("hit","cog",[]string{"hot","dot","dog","lot","log","cog"}))
+func Test_ladderLength(t *testing.T) {
+	fmt.Println(ladderLength("hit", "cog", []string{"hot", "dot", "dog", "lot", "log", "cog"}))
 }
 
-func Test_sortByBits(t *testing.T)  {
-	fmt.Println(sortByBits([]int{2,3,5,7,11,13,17,19}))
+func Test_sortByBits(t *testing.T) {
+	fmt.Println(sortByBits([]int{2, 3, 5, 7, 11, 13, 17, 19}))
 }
 
-func Test_countRangeSum(t *testing.T)  {
-	fmt.Println(countRangeSum([]int{-2,5,-1},-2,2))
+func Test_countRangeSum(t *testing.T) {
+	fmt.Println(countRangeSum([]int{-2, 5, -1}, -2, 2))
 }
 
-func Test_swap(t *testing.T)  {
-	a,b := 1,2
-	fmt.Println(a,b)
+func Test_swap(t *testing.T) {
+	a, b := 1, 2
+	fmt.Println(a, b)
 }
 
-func Test_nextPermutation(t *testing.T)  {
-	nums := []int{1,2,3}
+func Test_nextPermutation(t *testing.T) {
+	nums := []int{1, 2, 3}
 	nextPermutation(nums)
 	fmt.Println(nums)
 }
-func Test_sortArrayByParityII(t *testing.T)  {
-	nums := []int{4,1,2,1}
+func Test_sortArrayByParityII(t *testing.T) {
+	nums := []int{4, 1, 2, 1}
 	fmt.Println(sortArrayByParityII(nums))
 }
 
-func Test_relativeSortArray(t *testing.T)  {
-	arr1 := []int{2,3,1,3,2,4,6,7,9,2,19}
-	arr2 := []int{2,1,4,3,9,6}
-	fmt.Println(relativeSortArray(arr1,arr2))
+func Test_relativeSortArray(t *testing.T) {
+	arr1 := []int{2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19}
+	arr2 := []int{2, 1, 4, 3, 9, 6}
+	fmt.Println(relativeSortArray(arr1, arr2))
 }
 
-func Test_canCompleteCircuit(t *testing.T)  {
-	gas := []int{2,3,4}
-	cost := []int{3,4,3}
-	fmt.Println(canCompleteCircuit(gas,cost))
+func Test_canCompleteCircuit(t *testing.T) {
+	gas := []int{2, 3, 4}
+	cost := []int{3, 4, 3}
+	fmt.Println(canCompleteCircuit(gas, cost))
 }
 
-func Test_moveZeros(t *testing.T)  {
-	nums := []int{0,1,0,3,12}
+func Test_moveZeros(t *testing.T) {
+	nums := []int{0, 1, 0, 3, 12}
 	moveZeroes(nums)
 	fmt.Println(nums)
+}
+
+func Test_findMinArrowShots(t *testing.T) {
+	points := [][]int{{1, 2}, {2, 3}, {3, 4}, {4, 5}}
+	fmt.Println(findMinArrowShots(points))
 }
---
+++ dailyProblem/findMinArrowShots.go
@@ -0,0 +1,38 @@
+package dailyProblem
+
+import "sort"
+
+/**
+https://leetcode-cn.com/problems/minimum-number-of-arrows-to-burst-balloons/
+*/
+func findMinArrowShots(points [][]int) int {
+	if len(points) == 0 {
+		return 0
+	}
+	res := 1
+	if len(points) == 1 {
+		return res
+	}
+	//实际题目就是查询重叠区间的个数，但是每次重叠都是要收紧区间的大小，以便能够使用一支箭就能射穿全部的气球
+	sort.Slice(points, func(i, j int) bool {
+		return points[i][0] < points[j][0]
+	})
+	h := points[0][1]
+	for i := 1; i < len(points); i++ {
+		if points[i][0] <= h {
+			//有重合部分,那就收紧
+			h = min(h, points[i][1])
+			continue
+		}
+		h = points[i][1]
+		res++
+	}
+	return res
+}
+
+func min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
