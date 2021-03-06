a61589cb195ee50e2ad7adf2790f30941aaa3c41
hearecho <1540302560@qq.com>
2021-03-06 16:56:36 +0800

    2021/3/6-每日一题-503-分手多少天了？

---
+++ dailyProblem/nextGreaterElements.go
@@ -0,0 +1,20 @@
+package dailyProblem
+
+//单调栈
+//每次入栈 都将比此时栈顶值更小的值都
+func nextGreaterElements(nums []int) []int {
+	n := len(nums)
+	ans := make([]int, n)
+	for i := range ans {
+		ans[i] = -1
+	}
+	stack := []int{}
+	for i := 0; i < n*2-1; i++ {
+		for len(stack) > 0 && nums[stack[len(stack)-1]] < nums[i%n] {
+			ans[stack[len(stack)-1]] = nums[i%n]
+			stack = stack[:len(stack)-1]
+		}
+		stack = append(stack, i%n)
+	}
+	return ans
+}
