442f9bca3540d95ae20d94b0577bcbe5a1ee8f29
hearecho <1540302560@qqq.com>
2021-11-25 09:43:16 +0800

    2021-11-25

--- problem/math/Mypow/mypow.go
+++ problem/math/Mypow/mypow.go
@@ -1,20 +1,41 @@
 package Mypow
 
-
 func myPow(x float64, n int) float64 {
-	if n==0 {
+	if n == 0 {
 		return 1
 	}
-	if n==1 {
+	if n == 1 {
 		return x
 	}
-	if n < 0{
+	if n < 0 {
 		n = -n
-		x = 1/x
+		x = 1 / x
 	}
-	temp := myPow(x,n/2)
-	if n % 2 == 0 {
-		return temp *temp
+	temp := myPow(x, n/2)
+	if n%2 == 0 {
+		return temp * temp
 	}
-	return temp * temp *x
+	return temp * temp * x
+}
+
+//迭代版本 不使用递归
+func myPowIter(x float64, n int) float64 {
+	res := 1.0
+	if n == 0 {
+		return res
+	}
+	if n < 0 {
+		n = -n
+		x = 1 / x
+	}
+	for n > 0 {
+		if n%2 != 0 {
+			// 奇数先乘以x
+			res *= x
+		}
+		// 逐步转化为 更大的数的更小的幂运算。
+		x *= x
+		n /= 2
+	}
+	return res
 }
--- problem/math/Mypow/mypow_test.go
+++ problem/math/Mypow/mypow_test.go
@@ -10,13 +10,17 @@ type param struct {
 	n int
 }
 
-func Test_Mypow(t *testing.T)  {
+func Test_Mypow(t *testing.T) {
 	params := []param{
-		{2.00000,10},
-		{2.10000,3},
-		{2.00000,-2},
+		{2.00000, 10},
+		{2.10000, 3},
+		{2.00000, -2},
 	}
-	for _,p := range params {
-		fmt.Printf("【input】:%v\t【ouput】:%v\n",p,myPow(p.x,p.n))
+	for _, p := range params {
+		fmt.Printf("【input】:%v\t【ouput】:%v\n", p, myPow(p.x, p.n))
 	}
+}
+
+func TestFuncIter(t *testing.T) {
+	fmt.Println(myPowIter(2, 10))
 }
